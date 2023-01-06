979dcc3785b27e49211e0022b500396bf0e3a705
Swapnil1997 <kasimsaifi8826@gmail.com>
2023-01-06 17:30:20 +0530

    change this code

--- enroll/models.py
+++ enroll/models.py
@@ -4,4 +4,5 @@ class Image(models.Model):
    photo=models.ImageField(upload_to="myimage") #yanha store hoga
    date=models.DateTimeField(auto_now_add=True)
 
-
+   def __str__(self):
+      return self.date
